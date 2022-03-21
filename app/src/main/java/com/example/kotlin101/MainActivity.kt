bfa2665b6886420faa1944fb383d64000fbe2aa1
Alejandro <alexelninja@gmail.com>
2022-03-21 17:08:54 +0100

    xml images performance tweaks

--- app/src/main/java/com/example/kotlin101/MainActivity.kt
+++ app/src/main/java/com/example/kotlin101/MainActivity.kt
@@ -6,26 +6,28 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var rollButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.main_activity)
 
-        val rollButton: Button = findViewById(R.id.roll_dice_button)
+        diceImage = findViewById(R.id.dice_image)
+        rollButton = findViewById(R.id.roll_dice_button)
+
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-
-        val drawableResource = when ((1..6).random()) {
+        diceImage.setImageResource(when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
-        }
-
-        diceImage.setImageResource(drawableResource)
+        })
     }
 }
