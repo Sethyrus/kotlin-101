8a2c494d93bf3117d8d03d06f8cfdeb7b4beb661
Alejandro <alexelninja@gmail.com>
2022-03-21 16:43:30 +0100

    text dice changed to image

--- app/src/main/java/com/example/kotlin101/MainActivity.kt
+++ app/src/main/java/com/example/kotlin101/MainActivity.kt
@@ -3,21 +3,29 @@ package com.example.kotlin101
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
 
         val rollButton: Button = findViewById(R.id.roll_dice_button)
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.roll_dice_value)
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
