8dd25c3430b7b3a2805234c51f47429834ac34d6
Alejandro <alexelninja@gmail.com>
2022-03-21 13:58:01 +0100

    added basic dice roll

--- app/src/main/java/com/example/kotlin101/MainActivity.kt
+++ app/src/main/java/com/example/kotlin101/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.kotlin101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_dice_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+        val resultText: TextView = findViewById(R.id.roll_dice_value)
+        resultText.text = randomInt.toString()
     }
 }
